d42bfdf7b63438e4380ed57e371ea89e8e0ba780
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-31 13:29:44 -0500

    update has_many associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :question
   belongs_to :comment
+  has_many:comments
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  has_many :likes
 end
