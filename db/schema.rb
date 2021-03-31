a039520949ea20378132440fd78d0fba1c8348d5
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-03-31 14:08:47 -0500

    updating model comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_31_182151) do
+ActiveRecord::Schema.define(version: 2021_03_31_190513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "comment"
+    t.string "post"
     t.bigint "user_id", null: false
     t.bigint "question_id", null: false
-    t.bigint "comment_id", null: false
+    t.bigint "comment_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["comment_id"], name: "index_comments_on_comment_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@admin = User.create!(username: 'admin', email: 'ghudachek@gmail.com',image: 'https://images.unsplash.com/photo-1466921583968-f07aa80c526e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80', password: 'hello')
+
+puts "#{User.count} users created"
+
+@wyr1 = Question.create!(choice1: 'live at the beach', choice2: 'live in the mountains', user: @admin)
+@wyr2 = Question.create!(choice1: 'explore the ocean', choice2: 'explore space', user: @admin)
+
+puts "#{Question.count} questions created!"
+
+@com1 = Comment.create!(post: 'tough choices!', user: @admin, question: @wyr1)
+
+puts "#{Comment.count} comments created!"
