b491849b7a536bcc82351125fb7a4be4a3c5c9c2
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-04-01 16:27:25 -0500

    editing the layout carousel

--- client/src/layout/Layout.jsx
+++ client/src/layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../designs/Layout.css";
 
 export default function Layout(props) {
   const { currentUser, handleLogout } = props;
   return (
-    <div>
+    <div className="layout">
       <header>
         {currentUser ? (
           <>
--- client/src/screens/Landing.jsx
+++ client/src/screens/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 import "../designs/Landing.css";
+import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 const Landing = (props) => {
   console.log(props.questions);
@@ -16,28 +17,32 @@ const Landing = (props) => {
         <div>
           <Carousel>
             <Carousel.Item>
-              <div className="d-block w-100">
+              <div className="d-block">
                 <button className="block1">{props.questions[0].choice1}</button>
                 <button className="block2">{props.questions[0].choice2}</button>
+                <div className="info-wyr">
+                  <h3>
+                    Would you rather {props.questions[0].choice1} or{" "}
+                    {props.questions[0].choice2}
+                  </h3>
+                </div>
               </div>
-              <Carousel.Caption>
-                <h2>Would you rather...</h2>
-                <h3>
-                  {props.questions[0].choice1} or {props.questions[0].choice2}
-                </h3>
-              </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
-              <div className="d-block w-100">
+              <div className="d-block">
                 <button className="block1">{props.questions[1].choice1}</button>
                 <button className="block2">{props.questions[1].choice2}</button>
+                <div className="info-wyr">
+                  <h3>
+                    Would you rather {props.questions[1].choice1} or{" "}
+                    {props.questions[1].choice2}
+                  </h3>
+                  <Link to="/join">
+                    <button className="join">Join The Debate</button>
+                  </Link>
+                </div>
               </div>
-              <Carousel.Caption>
-                <h2>Would you rather...</h2>
-                <h3>
-                  {props.questions[1].choice1} or {props.questions[1].choice2}
-                </h3>
-              </Carousel.Caption>
+              s
             </Carousel.Item>
           </Carousel>
         </div>
