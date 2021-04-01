bbdba2305ca1c4f665853a93396dbc9632f907bd
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-04-01 12:12:59 -0500

    home page started rendering

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import {} from "module";
-
+import "./Home.css";
 const Home = (props) => {
-  return <div>This is the Home page</div>;
+  return (
+    <>
+      {props.questions.length !== 0 ? (
+        <div>
+          <p>This is the Home page</p>
+          {props.questions.map((question) => (
+            <>
+              <h2>Would You Rather:</h2>
+              <h3>
+                {question.choice1} or {question.choice1}
+              </h3>
+              <button className="choice">{question.choice1}</button>
+              <button className="choice">{question.choice2}</button>
+              <button className="likes">Like</button>
+            </>
+          ))}
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
+    </>
+  );
 };
 
 export default Home;
