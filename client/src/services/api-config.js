6cf1987abb44e7eb1e736799a879551b9fbb9702
Gracen hudachek <gracenhudachek@Gracens-MBP.attlocal.net>
2021-04-13 12:32:54 -0400

    update config file for production only

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://wouldyourather-api.herokuapp.com"
-    : "http://localhost:3000";
+const baseUrl = "https://wouldyourather-api.herokuapp.com";
+// process.env.NODE_ENV === "production"
+//   ? "https://wouldyourather-api.herokuapp.com"
+//   : "http://localhost:3000";
 
 const api = axios.create({
   baseURL: baseUrl,
